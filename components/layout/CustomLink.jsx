0a8da5055d4c0c863224faeec296e66e4c1b28d1
Sergey <finnesko7@gmail.com>
2020-07-02 17:28:57 +0300

    resolve warning with link

--- components/layout/CustomLink.jsx
+++ components/layout/CustomLink.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const CustomLink = (props) => {
     return (
-        <Link href={props.to} className="metismenu-link" >
+        <Link href={props.to}  >
             <a className="metismenu-link">{props.children}</a>
         </Link>
     );
